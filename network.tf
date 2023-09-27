a9e3a46aa9dbd95a799b4f0723bbff907b80dcce
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-09-27 22:47:30 +0530

    netowrk file updated

---
+++ network.tf
@@ -0,0 +1,18 @@
+# create vpc
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "172.16.1.0/24"
+
+}
+
+# create subnet
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "172.16.1.0/26"
+
+  tags = {
+    env  = "dev"
+  }
+}
+

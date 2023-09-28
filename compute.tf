1885fe3e400b3beaba9fc1a2bca69dc52ae6a381
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-09-28 12:34:14 +0530

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "ec2" {
   count         = 4
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
-  key_name      = "lab-key"
+  key_name      = "newkey"
 
   network_interface {
     network_interface_id = aws_network_interface.eni[count.index].id

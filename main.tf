28ff7a5eee44eb927bfe6d5cb09ff1737c4a4e68
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-10-02 08:21:23 +0530

    ansible inventory file

--- main.tf
+++ main.tf
@@ -6,14 +6,19 @@ provider "aws" {
 # Generate ansible inventory file
 
 resource "local_file" "ansible_inventory" {
-    count = 4
     content     =  templatefile(
                     "${path.cwd}/ansible/hosts.tftpl", {
-                     hostname= "worker", workernode_ip =  "${element(data.aws_network_interface.eni[count.index].private_ip, count.index)}"                   
+                     hostname= "worker", workernode_ip =  "${data.aws_network_interface.eni.*.private_ip}"                   
                      })
     filename    = "${path.cwd}/hosts"
     depends_on = [aws_instance.ec2,]
 }
 
-
-
+# resource "local_file" "ansible_inventory" {
+#     content     =  templatefile(
+#                     "${path.cwd}/hosts.tftpl", {
+#                      hostname = "AZUVNLABFGT00", fortigate_ip =  "${data.azurerm_public_ip.PublicIP.*.ip_address}",                   
+#                      })
+#     filename    = "${path.cwd}/hosts.ini"
+#     depends_on = [time_sleep.wait_180_seconds,azurerm_virtual_machine.fortigate_vm,]
+# }

3ad87b6511691eb21c24e0f625d9e9c718c4782a
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-10-02 08:12:19 +0530

    ansible inventory file

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "local_file" "ansible_inventory" {
     count = 4
     content     =  templatefile(
                     "${path.cwd}/ansible/hosts.tftpl", {
-                     hostname= "worker", workernode_ip =  "${element(data.aws_network_interface.eni.private_ip, count.index)}"                   
+                     hostname= "worker", workernode_ip =  "${element(data.aws_network_interface.eni.*.private_ip, count.index)}"                   
                      })
     filename    = "${path.cwd}/hosts"
     depends_on = [aws_instance.ec2,]

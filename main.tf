d9f1a70e85187816a5d243d4ef742a71da295d08
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-10-03 09:04:19 +0530

    ansible inventory

--- main.tf
+++ main.tf
@@ -3,14 +3,6 @@ provider "aws" {
 }
 
 
-# Generate ansible inventory file
 
-resource "local_file" "ansible_inventory" {
-    content     =  templatefile(
-                    "${path.cwd}/ansible/hosts.tftpl", {
-                     hostname= "worker", workernode_ip =  "${data.aws_network_interface.k8worker.*.private_ip}",
-                     k8master_ip = "${data.aws_network_interface.k8master.*.private_ip}"                
-                     })
-    filename    = "${path.cwd}/hosts"
-    depends_on = [aws_instance.k8master,aws_instance.k8worker]
-}
+
+

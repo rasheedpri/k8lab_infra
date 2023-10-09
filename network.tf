563cbf96790c6ff3d5e4c6d722f400a45d01f0bc
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-10-09 21:39:27 +0530

    nat gateway configuration

--- network.tf
+++ network.tf
@@ -133,10 +133,10 @@ resource "aws_route_table" "public" {
 
 }
 
-# route table assosiation dev subnet
+# route table assosiation public subnet
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.priv_subnet.id
+  subnet_id      = aws_subnet.pub_subnet.id
   route_table_id = aws_route_table.public.id
 }
 

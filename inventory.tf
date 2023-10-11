a04f7500d3b4a28558f9b9215d852dbca7f33674
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-10-11 18:59:49 +0530

    k8master removed for tshoot

--- inventory.tf
+++ inventory.tf
@@ -4,9 +4,9 @@ resource "local_file" "ansible_inventory" {
     content     =  templatefile(
                     "${path.cwd}/ansible/hosts.tftpl", {
                      hostname= "worker", workernode_ip =  "${data.aws_network_interface.k8worker.*.private_ip}",
-                     k8master_ip = "${data.aws_network_interface.k8master.*.private_ip}"  
+                     k8master_ip = "${data.aws_network_interface.k8master.*.private_ip}",  
                      jenkins_ip = "${data.aws_network_interface.jenkins.*.private_ip}"              
                      })
     filename    = "${path.cwd}/ansible/hosts"
-    depends_on = [aws_instance.k8master,aws_instance.k8worker,aws_instance.jenkins]
+    depends_on = [aws_instance.k8worker,aws_instance.jenkins]
 }

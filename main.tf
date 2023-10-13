5af124209390a958faf067e38fbb3d2841fb3ef5
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-10-13 16:05:39 +0530

    moving to CloudGuru

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5UFXVCBBV3EPAW6W"
+  access_key = "AKIA3UIJNQOMKU5DQN2V"
   secret_key = "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"
 }
-

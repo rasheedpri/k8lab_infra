1ea67e5ab02c6710e5f4802f8f42c0f565bc7b96
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-10-20 16:59:14 +0530

    cloud gure cred updated

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAUZ6UIKB7PML2IHMT"
+  access_key = "AKIAUNJWVR5ZJP5CVQGC"
   secret_key = "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"
 }
-

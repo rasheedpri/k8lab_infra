9e405b2d0ff7b2f5018aa8b0306a49c712c3d731
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-10-28 09:43:48 +0530

    lb added

--- lb.tf
+++ lb.tf
@@ -13,30 +13,30 @@
 } 
 
 
-resource "aws_lb_target_group" "nginx" {
+resource "aws_lb_target_group" "k8worker" {
   name     = "tf-example-lb-tg"
   port     = 32524
-  protocol = "TCP"
+  protocol = "HTTP"
   vpc_id   = aws_vpc.vpc.id
 }
 
 
 
-resource "aws_lb_listener" "nginx" {
+resource "aws_lb_listener" "webapp" {
   load_balancer_arn = aws_lb.lb.arn
   port              = "80"
   protocol          = "TCP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.nginx.arn
+    target_group_arn = aws_lb_target_group.k8worker.arn
   }
 }
 
 
 resource "aws_lb_target_group_attachment" "test" {
   count = 2
-  target_group_arn = aws_lb_target_group.nginx.arn
+  target_group_arn = aws_lb_target_group.k8worker.arn
   target_id        = aws_instance.k8worker[count.index].id
   port             = 32524
 }

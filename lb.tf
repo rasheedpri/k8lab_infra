62ee166d5f3ab2a33ed8ba08ba00c654cc055bc3
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-10-30 17:33:51 +0530

    lb added

--- lb.tf
+++ lb.tf
@@ -13,26 +13,26 @@
 } 
 
 
-resource "aws_lb_target_group" "k8worker" {
-  name     = "k8workers"
-  target_type = "instance"
-  port     = 32577
-  protocol = "TCP"
-  vpc_id   = aws_vpc.vpc.id
-}
+# resource "aws_lb_target_group" "k8worker" {
+#   name     = "k8workers"
+#   target_type = "instance"
+#   port     = 32577
+#   protocol = "TCP"
+#   vpc_id   = aws_vpc.vpc.id
+# }
 
 
 
-resource "aws_lb_listener" "webapp" {
-  load_balancer_arn = aws_lb.elb.arn
-  port              = "80"
-  protocol          = "TCP"
+# resource "aws_lb_listener" "webapp" {
+#   load_balancer_arn = aws_lb.elb.arn
+#   port              = "80"
+#   protocol          = "TCP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.k8worker.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.k8worker.arn
+#   }
+# }
 
 
 resource "aws_lb_target_group_attachment" "test" {

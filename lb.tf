b0ee39d2b511c2c93a944e3ada2c247886c1147c
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-10-30 19:24:37 +0530

    lb added

--- lb.tf
+++ lb.tf
@@ -13,33 +13,33 @@
 } 
 
 
-# resource "aws_lb_target_group" "k8worker" {
-#   name     = "k8workers"
-#   target_type = "instance"
-#   port     = 32577
-#   protocol = "TCP"
-#   vpc_id   = aws_vpc.vpc.id
-# }
+resource "aws_lb_target_group" "k8worker" {
+  name     = "k8workers"
+  target_type = "instance"
+  port     = 32457
+  protocol = "TCP"
+  vpc_id   = aws_vpc.vpc.id
+}
 
 
 
-# resource "aws_lb_listener" "webapp" {
-#   load_balancer_arn = aws_lb.elb.arn
-#   port              = "80"
-#   protocol          = "TCP"
+resource "aws_lb_listener" "webapp" {
+  load_balancer_arn = aws_lb.elb.arn
+  port              = "80"
+  protocol          = "TCP"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.k8worker.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.k8worker.arn
+  }
+}
 
 
-# resource "aws_lb_target_group_attachment" "test" {
-#   count = 2
-#   target_group_arn = aws_lb_target_group.k8worker.arn
-#   target_id        = aws_instance.k8worker[count.index].id
-#   port             = 32577
-# }
+resource "aws_lb_target_group_attachment" "test" {
+  count = 2
+  target_group_arn = aws_lb_target_group.k8worker.arn
+  target_id        = aws_instance.k8worker[count.index].id
+  port             = 32457
+}
 
 

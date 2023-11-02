d9ebd28686d0eb8540d72c8525d99f987953d38f
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-11-02 10:33:30 +0530

    subnet added for alb

--- compute.tf
+++ compute.tf
@@ -138,7 +138,7 @@ resource "aws_instance" "k8master" {
 
 # ec2 instace for k8 worker nodes in AZ a
 
-resource "aws_instance" "k8workers_az_b" {
+resource "aws_instance" "k8workers_az_a" {
   count         = 1
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"

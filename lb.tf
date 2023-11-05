e613c2d8d928782ddd45ee01531dfe2561ab2247
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-11-05 21:53:45 +0530

    nat enabled temp

--- lb.tf
+++ lb.tf
@@ -1,13 +1,13 @@
-# create alb target group for k8workers
+# # create alb target group for k8workers
 
-resource "aws_lb_target_group" "k8workers" {
-  name     = "k8workers"
-  port     = 31975
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.vpc.id
-  target_type = "instance"
+# resource "aws_lb_target_group" "k8workers" {
+#   name     = "k8workers"
+#   port     = 31975
+#   protocol = "HTTP"
+#   vpc_id   = aws_vpc.vpc.id
+#   target_type = "instance"
   
-}
+# }
 
 
 # resource "aws_lb" "alb" {
@@ -23,39 +23,39 @@ resource "aws_lb_target_group" "k8workers" {
 # }
 
 
-# attach  k8 worker nodes in AZ a
+# # attach  k8 worker nodes in AZ a
 
 
 
-resource "aws_lb_target_group_attachment" "k8workers_az_a" {
-  count = 1
-  target_group_arn = aws_lb_target_group.k8workers.arn
-  target_id        = aws_instance.k8workers_az_a[count.index].id
-  port             = 31975
-}
+# resource "aws_lb_target_group_attachment" "k8workers_az_a" {
+#   count = 1
+#   target_group_arn = aws_lb_target_group.k8workers.arn
+#   target_id        = aws_instance.k8workers_az_a[count.index].id
+#   port             = 31975
+# }
 
 
-# attach  k8 worker nodes in AZ b
+# # attach  k8 worker nodes in AZ b
 
 
 
-resource "aws_lb_target_group_attachment" "k8workers_az_b" {
-  count = 1
-  target_group_arn = aws_lb_target_group.k8workers.arn
-  target_id        = aws_instance.k8workers_az_b[count.index].id
-  port             = 31975
-}
+# resource "aws_lb_target_group_attachment" "k8workers_az_b" {
+#   count = 1
+#   target_group_arn = aws_lb_target_group.k8workers.arn
+#   target_id        = aws_instance.k8workers_az_b[count.index].id
+#   port             = 31975
+# }
 
 
-# creat listener for k8 workers
+# # creat listener for k8 workers
 
-resource "aws_lb_listener" "k8workers" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "443"
-  protocol          = "HTTPS"
+# resource "aws_lb_listener" "k8workers" {
+#   load_balancer_arn = aws_lb.alb.arn
+#   port              = "443"
+#   protocol          = "HTTPS"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.k8workers.arn
-  }
-  }
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.k8workers.arn
+#   }
+#   }

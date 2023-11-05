35f2f3a08f551e38a9ed8f2c57f060cfcbb4b5b9
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-11-05 08:34:46 +0530

    alb added

--- security.tf
+++ security.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "management" {
 
   ingress {
     description = "SSH from Jenkins Master"
-    from_port   = 0
-    to_port     = 65534
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -18,21 +18,57 @@ resource "aws_security_group" "management" {
 }
 
 
-resource "aws_security_group" "allow_http" {
-  name        = "http_inbound_sg"
-  description = "Allow inbound management traffic"
+resource "aws_security_group" "worker_inbound" {
+  name        = "management_sg"
+  description = "Allow web traffic"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
+    description = "web from alb"
+    from_port   = 31795
+    to_port     = 31795
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "SSH from Jenkins Master"
-    from_port   = 0
-    to_port     = 65534
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
 
   tags = {
-    Name = "Allow inbound  management traffic"
+    Name = "Allow inbound  web traffic"
+  }
+}
+
+
+
+resource "aws_security_group" "allow_http" {
+  name        = "http_inbound_sg"
+  description = "Allow inbound http traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "allow http from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 31795
+    to_port          = 31795
+    protocol         = "tcp"
+    security_groups      = [aws_security_group.worker_inbound.id]
+  }
+
+  tags = {
+    Name = "Allow inbound  http traffic"
   }
 }

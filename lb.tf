2ae637f26c937e250abf46ee56ade58ce816ebbb
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-11-06 17:27:57 +0530

    nat enabled temp

--- lb.tf
+++ lb.tf
@@ -1,50 +1,50 @@
-# create alb target group for k8workers
+# # create alb target group for k8workers
 
-resource "aws_lb_target_group" "k8workers" {
-  name     = "k8workers"
-  port     = 31975
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.vpc.id
-  target_type = "instance"
+# resource "aws_lb_target_group" "k8workers" {
+#   name     = "k8workers"
+#   port     = 31975
+#   protocol = "HTTP"
+#   vpc_id   = aws_vpc.vpc.id
+#   target_type = "instance"
   
-}
+# }
 
 
-resource "aws_lb" "alb" {
-  name               = "weblb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb.id]
-  subnets            = [aws_subnet.pub_subnet1.id,aws_subnet.pub_subnet2.id]
+# resource "aws_lb" "alb" {
+#   name               = "weblb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.alb.id]
+#   subnets            = [aws_subnet.pub_subnet1.id,aws_subnet.pub_subnet2.id]
 
-  tags = {
-    Environment = "dev"
-  }
-}
-
-
-# attach  k8 worker nodes in AZ a
+#   tags = {
+#     Environment = "dev"
+#   }
+# }
 
 
-
-resource "aws_lb_target_group_attachment" "k8workers_az_a" {
-  count = 1
-  target_group_arn = aws_lb_target_group.k8workers.arn
-  target_id        = aws_instance.k8workers_az_a[count.index].id
-  port             = 31975
-}
-
-
-# attach  k8 worker nodes in AZ b
+# # attach  k8 worker nodes in AZ a
 
 
 
-resource "aws_lb_target_group_attachment" "k8workers_az_b" {
-  count = 1
-  target_group_arn = aws_lb_target_group.k8workers.arn
-  target_id        = aws_instance.k8workers_az_b[count.index].id
-  port             = 31975
-}
+# resource "aws_lb_target_group_attachment" "k8workers_az_a" {
+#   count = 1
+#   target_group_arn = aws_lb_target_group.k8workers.arn
+#   target_id        = aws_instance.k8workers_az_a[count.index].id
+#   port             = 31975
+# }
+
+
+# # attach  k8 worker nodes in AZ b
+
+
+
+# resource "aws_lb_target_group_attachment" "k8workers_az_b" {
+#   count = 1
+#   target_group_arn = aws_lb_target_group.k8workers.arn
+#   target_id        = aws_instance.k8workers_az_b[count.index].id
+#   port             = 31975
+# }
 
 
 # # creat listener for k8 workers

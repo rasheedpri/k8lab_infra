45c0fefef9412156914a34fc5337f7d3fb252329
Abdul Rasheed <55779561+rasheedpri@users.noreply.github.com>
2023-12-07 14:17:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
-provider "aws" {}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
